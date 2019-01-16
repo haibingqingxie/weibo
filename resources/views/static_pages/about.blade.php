7374821112284d767983e9afea71967febf11645
haibingqingxie <jasonli@seasidecrab.com>
2019-01-16 07:10:17 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '关于页')
+@section('content')
+  <h1>关于页</h1>
+  <p>继承layouts.default视图</p>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'Weibo App')
+@section('content')
+  <h1>主页</h1>
+  <p>继承layouts.default视图</p>
+@stop
