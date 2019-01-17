ea4ba3adeb25fba59b84fc3a2328994a50f85deb
haibingqingxie <jasonli@seasidecrab.com>
2019-01-17 03:23:03 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function gravatar($size = 100)
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "https://www.gravatar.com/avater/$hash?s=$size";
+    }
 }
