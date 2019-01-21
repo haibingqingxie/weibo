e901609358e8f75c519d873f5ccc4f6813cff683
haibingqingxie <jasonli@seasidecrab.com>
2019-01-21 10:16:18 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,38 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
     }
+
+    public function followers()
+    {
+        // 参数1为关联的模型，参数2为关联关系表名，参数3为关联的模型外键名，参数4为要合并的模型外键名
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
+    }
 }
